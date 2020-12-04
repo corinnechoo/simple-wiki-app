417d87ab6b9b1f67606384200273f8357f064311
Corinne Choo <corinne_choo@nsearchglobal.com>
2020-12-04 19:37:02 +0800

    Add mysql connector

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+			type: 'mysql',
+			host: process.env.DB_HOST,
+			port: Number(process.env.DB_PORT),
+			username: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_DATABASE,
+			dateStrings: true,
+			autoLoadEntities: true,
+			entities: [],
+		})
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
