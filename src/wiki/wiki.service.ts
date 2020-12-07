15acc4f52b36cb18b8812b34112908e9a7f72556
Corinne Choo <corinne_choo@nsearchglobal.com>
2020-12-07 13:07:39 +0800

    fix sql query

--- src/wiki/wiki.service.ts
+++ src/wiki/wiki.service.ts
@@ -50,20 +50,23 @@ export class WikiService {
      */
     // TODO: format response (do we want page title to be space separated? convert seconds to days?)
     getOutdatedPages(category): Promise<any> {
+        const cleanCategory = category.replace("'", "\'")
         const sql = `
-        SELECT CAST(c1.category AS CHAR(100)) AS category, c1.page_id, c1.time_stamp_diff, CAST(p.page_title AS CHAR(100)) page_title
-        FROM page p,
+        SELECT
+            co.page_id, co.time_stamp_diff, CAST(p.page_title AS CHAR(100)) page_title, p.page_last_modified 
+        FROM
+            categoryoutdatedness co,
+            page p
+        WHERE co.time_stamp_diff in 
         (
-           SELECT c2.page_id , MAX(c2.time_stamp_diff) maxdiff
-           FROM categoryoutdatedness c2 
-           WHERE c2.category = '${category}'
-           GROUP BY c2.category 
-        ) c2 
-        INNER JOIN  categoryoutdatedness c1
-            ON c1.category = '${category}' AND c1.time_stamp_diff = c2.maxdiff
-        WHERE p.page_id = c1.page_id;
+            SELECT MAX(c.time_stamp_diff) maxdiff
+            FROM categoryoutdatedness c 
+            WHERE c.category = '${cleanCategory}'
+            GROUP BY c.category)
+            AND p.page_id = co.page_id
         `
         let response = this.connection.query(sql)
         return response
     }
 }
+
